4f46046daf1a159b020fb672360ab61aa63a79c1
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-09-03 16:54:28 +0900

    테일윈드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
+import { Music, Monitor } from 'lucide-react';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showButtons, setShowButtons] = useState(false);
+    let timeout: number;
+
+    const handleMouseMove = () => {
+        setShowButtons(true);
+        clearTimeout(timeout);
+        timeout = setTimeout(() => setShowButtons(false), 2000);
+    };
+
+    useEffect(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            clearTimeout(timeout);
+        };
+    }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-screen h-screen bg-gray-900">
+          <div
+              className={`fixed bottom-5 right-5 flex flex-col gap-2 opacity-0 transition-opacity duration-300 ease-in-out ${
+                  showButtons ? "opacity-100" : "opacity-0"
+              }`}
+          >
+              <button className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
+                  <Music color="white" size={24} />
+              </button>
+
+              <button className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
+                  <Monitor color="white" size={24} />
+              </button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
