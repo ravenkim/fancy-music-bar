9fde8f11207efb26e14344b36e16959dec4d3164
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-09-03 17:05:40 +0900

    레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,86 @@
-import {useEffect, useState} from 'react'
-import { Music, Monitor } from 'lucide-react';
+import {useCallback, useEffect, useRef, useState} from 'react'
+import {Music, Monitor} from 'lucide-react';
+import {Drawer} from 'vaul';
 
 import './App.css'
 
 function App() {
     const [showButtons, setShowButtons] = useState(false);
-    let timeout: number;
+    const timeoutRef = useRef<number | null>(null);
 
-    const handleMouseMove = () => {
+    const handleMouseMove = useCallback(() => {
         setShowButtons(true);
-        clearTimeout(timeout);
-        timeout = setTimeout(() => setShowButtons(false), 2000);
-    };
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+        timeoutRef.current = window.setTimeout(() => setShowButtons(false), 1000);
+    }, []);
 
     useEffect(() => {
         window.addEventListener('mousemove', handleMouseMove);
+
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
-            clearTimeout(timeout);
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
         };
-    }, []);
+    }, [handleMouseMove]);
 
-  return (
-      <div className="w-screen h-screen bg-gray-900">
-          <div
-              className={`fixed bottom-5 right-5 flex flex-col gap-2 opacity-0 transition-opacity duration-300 ease-in-out ${
-                  showButtons ? "opacity-100" : "opacity-0"
-              }`}
-          >
-              <button className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
-                  <Music color="white" size={24} />
-              </button>
+    return (
+        <div className="w-screen h-screen bg-gray-900">
+            <div
+                className={'w-full h-full '}
+            >
 
-              <button className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
-                  <Monitor color="white" size={24} />
-              </button>
-          </div>
-      </div>
-  )
+
+            </div>
+
+            <div
+                className={`fixed bottom-5 right-5 flex flex-col gap-2 opacity-0 transition-opacity duration-300 ease-in-out ${
+                    showButtons ? "opacity-100" : "opacity-0"
+                }`}
+            >
+
+
+                <Drawer.Root direction={"left"}>
+                    <Drawer.Trigger asChild>
+                        <button
+                            className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
+                            <Music color="white" size={24}/>
+                        </button>
+                    </Drawer.Trigger>
+                    <Drawer.Portal>
+                        <Drawer.Overlay className="fixed inset-0 bg-black/40"/>
+                        <Drawer.Content
+
+                            className="bg-gray-900 w-80 h-full fixed bottom-0 top-0 left-0 outline-none">
+
+                        </Drawer.Content>
+                    </Drawer.Portal>
+                </Drawer.Root>
+
+
+                <Drawer.Root direction={"left"}>
+                    <Drawer.Trigger asChild>
+                        <button
+                            className="bg-gray-800 rounded-full w-14 h-14 flex justify-center items-center shadow-md">
+                            <Monitor color="white" size={24}/>
+                        </button>
+                    </Drawer.Trigger>
+                    <Drawer.Portal>
+                        <Drawer.Overlay className="fixed inset-0 bg-black/40"/>
+                        <Drawer.Content
+
+                            className="bg-gray-900 w-80 h-full fixed bottom-0 top-0 left-0 outline-none">
+
+                        </Drawer.Content>
+                    </Drawer.Portal>
+                </Drawer.Root>
+
+            </div>
+        </div>
+    )
 }
 
 export default App
