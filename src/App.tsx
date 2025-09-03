5711854c44e120e27c8deef1edb4df4f2d3457dc
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-09-03 17:37:08 +0900

    배경 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import FloatingButtons from './components/FloatingButtons';
 
 import './App.css'
 import Prism from "./bg/Prism.tsx";
+import Hyperspeed from "./bg/hyperspeed/Hyperspeed.tsx";
+import {hyperspeedPresets} from "./bg/hyperspeed/hyperspeedPresets.ts";
 
 function App() {
 
@@ -18,17 +20,25 @@ function App() {
 
 
 
-                    <Prism
-                        animationType="rotate"
-                        timeScale={0.5}
-                        height={8}
-                        baseWidth={8}
-                        scale={2}
-                        hueShift={0}
-                        colorFrequency={1}
-                        noise={0}
-                        glow={1}
-                    />
+                    {/*<Prism*/}
+                    {/*    animationType="rotate"*/}
+                    {/*    timeScale={0.5}*/}
+                    {/*    height={8}*/}
+                    {/*    baseWidth={8}*/}
+                    {/*    scale={2}*/}
+                    {/*    hueShift={0}*/}
+                    {/*    colorFrequency={1}*/}
+                    {/*    noise={0}*/}
+                    {/*    glow={1}*/}
+                    {/*/>*/}
+
+                <Hyperspeed
+                    effectOptions={
+                        hyperspeedPresets.one
+                    }
+                />
+
+
             </div>
 
 
